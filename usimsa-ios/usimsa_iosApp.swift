50dc9626231334e46ec93e253cabf2b4267fc06d
Yeonju Lee <yeonju.lee1005@gmail.com>
2023-12-13 13:24:07 +0900

    Initial Commit

---
+++ usimsa-ios/usimsa_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  usimsa_iosApp.swift
+//  usimsa-ios
+//
+//  Created by 이연주 on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct usimsa_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
