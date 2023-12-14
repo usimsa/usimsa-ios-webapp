71464ceb4241497d70b45fc8f1f9acf97371d335
Yeonju Lee <yeonju.lee1005@gmail.com>
2023-12-14 10:26:16 +0900

    [usimsa-iso] FE-Improvement-026 (dewdew)
    - ios 웹뷰 QA 대응관련: userAgent 비정상 적용문제 해결

--- usimsa/ContentView.swift
+++ usimsa/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  webView
 //
-//  Created by 연주 on 2023/02/03.
+//  Created by 연주 on 2023/12/13.
 //
 
 import SwiftUI
@@ -10,7 +10,6 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         MyWebView(urlToLoad: "https://www.usimsa.com")
-            .edgesIgnoringSafeArea(.all)
             .scrollIndicators(/*@START_MENU_TOKEN@*/.never/*@END_MENU_TOKEN@*/, axes: /*@START_MENU_TOKEN@*/[.vertical, .horizontal]/*@END_MENU_TOKEN@*/)
     }
 }
